567e15dfc8646389d0b440a3003f337f984d4f96
Murillo Carvalho <murillohpcarvalho@gmail.com>
2026-05-27 23:30:52 -0300

    ultimos exercicios de iteraveis

--- iteraveis/exercicio_10.py
+++ iteraveis/exercicio_10.py
@@ -4,3 +4,15 @@ Crie um dicionário onde as chaves são os nomes de 3 alunos e os valores são
 listas contendo 4 notas de cada um. O programa deve calcular a média de cada 
 aluno e exibir o nome do aluno ao lado de sua média final.
 """
+
+notas = {"Ana": [1,2,3,4], "Carol": [5,6,7,8], "Vitoria": [9,10,10,10]}
+
+# medias = [(aluno, sum(nota)/4) for aluno, nota in notas.items()]
+
+medias = {aluno: sum(nota)/4 for aluno, nota in notas.items()}
+
+print(*medias.items(), sep="\n")
+
+"""
+CUIDADO: ao usar dict comprehension com {aluno, media for .....} cria um set invés de dicionario
+"""
--- iteraveis/exercicio_9.py
+++ iteraveis/exercicio_9.py
@@ -10,3 +10,33 @@ Buscar um telefone a partir do nome.
 Sair do programa.
 O programa deve rodar em um laço while até que a opção de sair seja escolhida.
 """
+import inquirer
+
+agenda_telefonica = {}
+choices = [
+    'Inserir um novo contato',
+    'Buscar um telefone a partir do nome',
+    'Mostrar agenda',
+    'Sair do programa'
+]
+while True:
+    questions = [
+        inquirer.List(
+            'opcao',
+            message="Escolha uma opção",
+            choices=choices),
+    ]
+    resposta = inquirer.prompt(questions)
+    if choices.index(resposta['opcao']) == 0:
+        nome = input("Digite o nome: ")
+        telefone = input("Digite o telefone: ")
+        agenda_telefonica[nome] = telefone
+    if choices.index(resposta['opcao']) == 1:
+        nome = input("Digite o nome: ")
+        print(*[(chave, dado) for chave, dado in agenda_telefonica.items() if chave == nome])
+    if choices.index(resposta['opcao']) == 2:
+        print(*agenda_telefonica.items())
+    if choices.index(resposta['opcao']) == 3:
+        print("Até a próxima!")
+        break
+    print("\n")
